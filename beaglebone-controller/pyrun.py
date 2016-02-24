1500765687cd978c1038221871c4c431d797622d
Ryan O'Shea <ryan@ryanoshea.com>
2016-02-23 22:53:24 -0500

    Removed calls to DB, added print statements for temp readings

--- beaglebone-controller/pyrun.py
+++ beaglebone-controller/pyrun.py
@@ -1,49 +1,52 @@
 
 import pytest6
-import pytest5 
+import pytest5
 import schedule
-import time 
-import smbus 
+import time
+import smbus
 import subprocess
 import mysql.connector
 import dbAdd
 from mysql.connector import Error
 
 #print "Content-type:text/html\r\n\r\n"
-#a = """<html><head><title>pyrun8 BBB</title></head> <body>""" 
+#a = """<html><head><title>pyrun8 BBB</title></head> <body>"""
 #print a
 
 #c = """<br>"""
 
 #for i in range(1,30):
 def pyrun():
-    f1 = open("output1.txt", "w") 
-    f2 = open("output2.txt", "w") 
-    
+    f1 = open("output1.txt", "w")
+    f2 = open("output2.txt", "w")
+
     subprocess.check_call(['/usr/sbin/i2cdetect', '-y', '-r', '1'], stdout=f1)
     subprocess.check_call(['/usr/sbin/i2cdetect', '-y', '-r', '2'], stdout=f2)
-    
-    #print "round " 
+
+    #print "round "
     #print i
     #print c
-    output1 = pytest6.sensorread() 
+    output1 = pytest6.sensorread()
     output2 = pytest5.sensorread()
-    
+
+    print(output1)
+    print(output2)
+
     output1.extend(output2)
-    
+
     #print output1
-    dbAdd.insertTemp(output1)
+    #               dbAdd.insertTemp(output1)
     #print c
-    #readable = time.asctime() 
+    #readable = time.asctime()
     #store = time.localtime()
     #print store
     #print c
     #print readable
-    #print int(time.time()) 
+    #print int(time.time())
     #print c
     #print c
-    subprocess.check_call(['rm', '-rf', 'output1.txt']) 
-    subprocess.check_call(['rm', '-rf', 'output2.txt']) 
+    subprocess.check_call(['rm', '-rf', 'output1.txt'])
+    subprocess.check_call(['rm', '-rf', 'output2.txt'])
 
 
 
@@ -52,5 +55,5 @@ schedule.every(0.5).seconds.do(pyrun)
 while 1:
     schedule.run_pending()
     time.sleep(1)
-    
+
 #time.sleep(5)
