67623d608c9dabda03355b698f04f54f5a1aa768
Ryan O'Shea <ryan@ryanoshea.com>
2016-03-21 19:26:42 -0400

    [Server] Established working server for backend and frontend

--- server-app/backend/server.js
+++ server-app/backend/server.js
@@ -4,13 +4,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var crypto = require('crypto');
+var compression = require('compression');
 var async = require('async');
 var fs = require('fs');
+var static = express();
 var app = express();
 
 // Configure POST body parsing
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Enable gzip
+app.use(compression());
+static.use(compression());
+
+// Direct everything except /api to the frontend application
+static.use(/^(?!\/api$).*/, express.static('../frontend'));
+
+// Mount app at /api
+static.use('/api', app);
 
 // MongoDB Setup
 var mongoose = require('mongoose');
@@ -33,7 +45,12 @@ var readingSchema = mongoose.Schema({
 
 var Reading = mongoose.model('Reading', readingSchema);
 
-// Request Handlers
+
+// Backend Request Handlers
+
+app.get('/', function (req, res) {
+  res.send('<h1>This is a request handler for the root of the backend application!</h1>');
+});
 
 app.post('/', function (req, res) {
   console.log('data received');
@@ -48,5 +65,5 @@ app.post('/', function (req, res) {
   res.end();
 });
 
-app.listen(3000);
+static.listen(80);
 console.log('Server running at http://localhost:3000');
