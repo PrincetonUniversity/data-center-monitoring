e63bb9ebee7dd5e824898a9d4c20a26ac7a0a0c9
Ryan O'Shea <ryan@ryanoshea.com>
2016-03-23 14:06:44 -0400

    [BB] Added exception handler for failed fork()
    This solution just ignores the error. If it happens frequently, it'll
    need to be addressed a different way to avoid lost readings.

--- beaglebone-controller/sensor-controller/com.py
+++ beaglebone-controller/sensor-controller/com.py
@@ -9,11 +9,14 @@ server = 'http://dcsense.ee.princeton.edu/api/sensors/submitreadings'
 def send_to_server(data):
 
     # Execute post request to server asynchronously
-    newpid = os.fork()
-    if newpid == 0:
-        try:
-            headers = {'content-type': 'application/json'}
-            requests.post(server, headers=headers, data=json.dumps(data))
-        except ConnectionError, err:
-            sys.stderr.write('Could not contact server:\n' + str(err))
-        sys.exit()
+    try:
+        newpid = os.fork()
+        if newpid == 0:
+            try:
+                headers = {'content-type': 'application/json'}
+                requests.post(server, headers=headers, data=json.dumps(data))
+            except ConnectionError, err:
+                sys.stderr.write('Could not contact server:\n' + str(err))
+            sys.exit()
+    except OSError, err:
+        sys.stderr.write('OSError on call to fork(): \n' + err)
