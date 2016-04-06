d3ae666faffe7b2ce8460ed557dc1aecbadf92e6
Ryan O'Shea <ryan@ryanoshea.com>
2016-04-05 20:33:55 -0400

    [Server] Fixed controller <select> default issue

--- server-app/frontend/app/directives/directives.js
+++ server-app/frontend/app/directives/directives.js
@@ -25,4 +25,18 @@ dir.directive('logout', function ($http, $location, $cookies) {
   }
 });
 
+dir.directive('convertToNumber', function() {
+  return {
+    require: 'ngModel',
+      link: function(scope, element, attrs, ngModel) {
+        ngModel.$parsers.push(function(val) {
+        return parseInt(val, 10);
+        });
+        ngModel.$formatters.push(function(val) {
+        return '' + val;
+        });
+      }
+  }
+});
+
 })();
