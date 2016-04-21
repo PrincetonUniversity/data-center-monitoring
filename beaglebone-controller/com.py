20eb7cb58dfaa7051b47e8ebe985123166188989
Ryan O'Shea <ryan@ryanoshea.com>
2016-04-21 16:02:49 -0400

    Revert "[BB] Configured request to go through proxy"
    This reverts commit 49809e4507e91491be8bd2739193c54c67e198bd.

--- beaglebone-controller/com.py
+++ beaglebone-controller/com.py
@@ -6,34 +6,24 @@ import os
 
 server = 'http://dcsense.ee.princeton.edu/api/sensors/submitreadings'
 
-
 def send_to_server(data, board_status):
-  # Execute post request to server asynchronously
-  try:
-    newpid = os.fork()
-    if newpid == 0:
-      try:
-        http_proxy = "http://128.112.48.208:3128"
-        https_proxy = "https://128.112.48.208:1080"
-        ftp_proxy = "ftp://128.112.48.208:3128"
 
-        proxyDict = {
-          "http": http_proxy,
-          "https": https_proxy,
-          "ftp": ftp_proxy
-        }
-
-        payload = {
-          'data': data,
-          'status': board_status
-        }
-        headers = {'content-type': 'application/json'}
-        requests.post(server, headers=headers, data=json.dumps(payload), proxies=proxyDict)
-      except ConnectionError, err:
-        sys.stderr.write('Could not contact server:\n' + str(err))
-      os._exit(0)  # Kill child process
-      print "Wasn't supposed to get here."
-    else:
-      os.waitpid(0, os.WNOHANG)  # Clear zombie child process
-  except OSError, err:
-    sys.stderr.write('OSError on call to fork(): \n' + err)
+    # Execute post request to server asynchronously
+    try:
+        newpid = os.fork()
+        if newpid == 0:
+            try:
+                payload = {
+                    'data': data,
+                    'status': board_status
+                }
+                headers = {'content-type': 'application/json'}
+                requests.post(server, headers=headers, data=json.dumps(payload))
+            except ConnectionError, err:
+                sys.stderr.write('Could not contact server:\n' + str(err))
+            os._exit(0) # Kill child process
+            print "Wasn't supposed to get here."
+        else:
+            os.waitpid(0, os.WNOHANG) # Clear zombie child process
+    except OSError, err:
+        sys.stderr.write('OSError on call to fork(): \n' + err)
